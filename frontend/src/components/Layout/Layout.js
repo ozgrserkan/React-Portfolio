1e17c3a0e2fa0e13bddf74b575227a7208589e3d
serkan <ozgr.serkan09@gmail.com>
2022-04-06 13:09:16 +0300

    sidebar added

---
+++ frontend/src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import Sidebar from '../Sidebar/Sidebar'
+import './Layout.scss'
+
+const Layout = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Layout
---
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,62 @@
+import './Sidebar.scss'
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/about"
+          className="about-link"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/contact"
+          className="contact-link"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/serkan-%C3%B6zg%C3%BCr-004177131/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/ozgrserkan?tab=repositories"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
