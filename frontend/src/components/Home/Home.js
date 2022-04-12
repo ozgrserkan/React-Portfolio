964cd97e5c6cc0654ce2f935d7932629ac05c4f7
serkan <ozgr.serkan09@gmail.com>
2022-04-12 12:17:33 +0300

    contact page added and project completed

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import Logo from './Logo/Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -17,35 +18,38 @@ const Home = () => {
   //   }, 4000)
   // })
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass}_12`}>i,</span>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass}_12`}>i,</span>
+            <br />
+            <span className={`${letterClass}_13`}>I</span>
+            <span className={`${letterClass}_14`}>'m</span>
+            <img src={LogoTitle} alt="Developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+          </h1>
           <br />
-          <span className={`${letterClass}_13`}>I</span>
-          <span className={`${letterClass}_14`}>'m</span>
-          <img src={LogoTitle} alt="Developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-        </h1>
-        <br />
-        <h2>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+          <h2>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
